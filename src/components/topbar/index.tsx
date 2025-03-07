27c9f5e6e8818dc8e2bbf83c209b1a64a9634c36
Harkesh Yadav <raoharkesh1998@gmail.com>
2025-03-07 08:36:26 +0530

    items added in topbar

--- src/components/topbar/index.tsx
+++ src/components/topbar/index.tsx
@@ -1,5 +1,15 @@
 export default function Topbar() {
   return (
-    <div className="w-full h-[50px] sticky top-0 bg-secondary px-10 py-4 flex justify-between items-center"></div>
+    <div className="w-full h-[50px] sticky top-0 bg-secondary px-10 py-4 flex justify-between items-center">
+      <div className="flex items-center gap-2">
+        <span className="text-white text-base">WebApp</span>
+      </div>
+      <div className="flex items-center gap-2">
+        <div></div>
+        <div>
+          <span className="text-white text-sm">User</span>
+        </div>
+      </div>
+    </div>
   );
 }
