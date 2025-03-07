52e231ab01e28b4769a74117b5ab3eae57efb29f
Harkesh Yadav <raoharkesh1998@gmail.com>
2025-03-07 21:40:18 +0530

    link added in logo

--- src/components/topbar/index.tsx
+++ src/components/topbar/index.tsx
@@ -1,8 +1,12 @@
+import Link from "next/link";
+
 export default function Topbar() {
   return (
     <div className="w-full h-[50px] sticky top-0 bg-secondary px-10 py-4 flex justify-between items-center">
       <div className="flex items-center gap-2">
-        <span className="text-white text-base">WebApp</span>
+        <Link href={"/"}>
+          <span className="text-white text-base">WebApp</span>
+        </Link>
       </div>
       <div className="flex items-center gap-2">
         <div></div>
