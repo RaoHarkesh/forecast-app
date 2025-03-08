6ab75dd539e1dfd87bc8c571e44842e876a0237a
Harkesh Yadav <raoharkesh1998@gmail.com>
2025-03-08 15:16:24 +0530

    checkbox added

---
+++ src/components/checkbox/index.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+import Checkbox from "@mui/material/Checkbox";
+
+const label = { inputProps: { "aria-label": "Checkbox demo" } };
+
+export default function CustomCheckbox() {
+  return (
+    <div>
+      <Checkbox
+        sx={{
+          color: "#8E979B", // Default border color
+        }}
+        {...label}
+      />
+    </div>
+  );
+}
