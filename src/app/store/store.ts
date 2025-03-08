0dfda4bd18e7531c7f08802eaba8f87e0314ea5b
Harkesh Yadav <raoharkesh1998@gmail.com>
2025-03-09 00:05:40 +0530

    redux store setup

--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -1,16 +1,14 @@
-import { configureStore } from '@reduxjs/toolkit'
-import { matrixSlice } from './matrix'
-import { gameSlice } from './game'
+import { configureStore } from "@reduxjs/toolkit";
+import { collapseSlice } from "./game";
 
 // Configure the Redux store
 export const store = configureStore({
   reducer: {
-    matrixData: matrixSlice.reducer, // Add the matrix slice to the store
-    gameData: gameSlice.reducer
+    collapseData: collapseSlice.reducer,
   },
-})
+});
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>
+export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {matrix: MatrixState}
-export type AppDispatch = typeof store.dispatch
+export type AppDispatch = typeof store.dispatch;
