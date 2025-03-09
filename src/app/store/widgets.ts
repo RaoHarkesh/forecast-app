d5e3bad7d0e2c8602cb9d78fd5fda124659dd201
Harkesh Yadav <raoharkesh1998@gmail.com>
2025-03-09 15:59:10 +0530

    type fix

--- src/app/store/widgets.ts
+++ src/app/store/widgets.ts
@@ -1,4 +1,4 @@
-import { createSlice, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 import { v4 as uuidv4 } from "uuid";
 
