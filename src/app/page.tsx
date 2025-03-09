d3cffdb4962f5c0ce41ab8cc3f2d2e7ea5ec5d56
Harkesh Yadav <raoharkesh1998@gmail.com>
2025-03-09 15:34:48 +0530

    added slice for widget

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
+"use client";
 import Button from "@/components/buttons";
 import Widget from "@/components/widgets";
 import WorldMap from "@/components/world-map/index";
 import MaxWidthWrapper from "@/components/wrappers";
+import { useAppDispatch, useAppSelector } from "./store/hooks";
+import { fetchWidgetData } from "./store/widgets";
+import { useEffect } from "react";
+import { Skeleton } from "@mui/material";
 
 const Home = () => {
+  const { loading, data } = useAppSelector((state) => state.widgetData);
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    dispatch(fetchWidgetData());
+  }, []);
   return (
     <>
       <section>
@@ -13,42 +23,69 @@ const Home = () => {
       </section>
       <MaxWidthWrapper>
         <section className="">
-          <div className="flex gap-4 mb-10 items-end">
+          <div className="flex flex-col md:flex-row gap-4 mb-10 md:items-end">
             <span className="text-white text-3xl font-bold">Hello user,</span>
             <Button customStyles="bg-primary">There are 2 action items</Button>
           </div>
           <div className="w-full flex justify-center">
             <div className="w-full overflow-x-auto no-scrollbar flex gap-5">
-              <Widget
-                forecastCount={45}
-                foreCastPercentage={65}
-                cityName="City 1"
-              />
-              <Widget
-                forecastCount={45}
-                foreCastPercentage={65}
-                cityName="City 2"
-              />
-              <Widget
-                forecastCount={45}
-                foreCastPercentage={65}
-                cityName="City 3"
-              />
-              <Widget
-                forecastCount={45}
-                foreCastPercentage={65}
-                cityName="City 4"
-              />
-              <Widget
-                forecastCount={45}
-                foreCastPercentage={65}
-                cityName="City 5"
-              />
-              <Widget
-                forecastCount={45}
-                foreCastPercentage={65}
-                cityName="City 6"
-              />
+              {loading ? (
+                <>
+                  <Skeleton
+                    sx={{ flex: "none" }}
+                    variant="rectangular"
+                    width={200}
+                    height={200}
+                  />
+                  <Skeleton
+                    sx={{ flex: "none" }}
+                    variant="rectangular"
+                    width={200}
+                    height={200}
+                  />
+                  <Skeleton
+                    sx={{ flex: "none" }}
+                    variant="rectangular"
+                    width={200}
+                    height={200}
+                  />
+                  <Skeleton
+                    sx={{ flex: "none" }}
+                    variant="rectangular"
+                    width={200}
+                    height={200}
+                  />
+                  <Skeleton
+                    sx={{ flex: "none" }}
+                    variant="rectangular"
+                    width={200}
+                    height={200}
+                  />
+                  <Skeleton
+                    sx={{ flex: "none" }}
+                    variant="rectangular"
+                    width={200}
+                    height={200}
+                  />
+                  <Skeleton
+                    sx={{ flex: "none" }}
+                    variant="rectangular"
+                    width={200}
+                    height={200}
+                  />
+                </>
+              ) : (
+                <>
+                  {data.map((ele) => (
+                    <Widget
+                      key={ele.id}
+                      forecastCount={ele.forecastCount}
+                      foreCastPercentage={ele.foreCastPercentage}
+                      cityName={ele.cityName}
+                    />
+                  ))}
+                </>
+              )}
             </div>
           </div>
         </section>
---
+++ src/app/store/widgets.ts
@@ -0,0 +1,103 @@
+import { createSlice, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit";
+
+import { v4 as uuidv4 } from "uuid";
+
+const dataArray = [
+  {
+    id: uuidv4(),
+    cityName: "City 1",
+    forecastCount: Math.floor(Math.random() * (200 - 50 + 1)) + 50,
+    foreCastPercentage: Math.floor(Math.random() * (90 - 10 + 1)) + 10,
+  },
+  {
+    id: uuidv4(),
+    cityName: "City 2",
+    forecastCount: Math.floor(Math.random() * (200 - 50 + 1)) + 50,
+    foreCastPercentage: Math.floor(Math.random() * (90 - 10 + 1)) + 10,
+  },
+  {
+    id: uuidv4(),
+    cityName: "City 3",
+    forecastCount: Math.floor(Math.random() * (200 - 50 + 1)) + 50,
+    foreCastPercentage: Math.floor(Math.random() * (90 - 10 + 1)) + 10,
+  },
+  {
+    id: uuidv4(),
+    cityName: "City 4",
+    forecastCount: Math.floor(Math.random() * (200 - 50 + 1)) + 50,
+    foreCastPercentage: Math.floor(Math.random() * (90 - 10 + 1)) + 10,
+  },
+  {
+    id: uuidv4(),
+    cityName: "City 5",
+    forecastCount: Math.floor(Math.random() * (200 - 50 + 1)) + 50,
+    foreCastPercentage: Math.floor(Math.random() * (90 - 10 + 1)) + 10,
+  },
+  {
+    id: uuidv4(),
+    cityName: "City 6",
+    forecastCount: Math.floor(Math.random() * (200 - 50 + 1)) + 50,
+    foreCastPercentage: Math.floor(Math.random() * (90 - 10 + 1)) + 10,
+  },
+  {
+    id: uuidv4(),
+    cityName: "City 7",
+    forecastCount: Math.floor(Math.random() * (200 - 50 + 1)) + 50,
+    foreCastPercentage: Math.floor(Math.random() * (90 - 10 + 1)) + 10,
+  },
+];
+
+interface WidgetState {
+  loading: boolean;
+  data: Array<{
+    id: string;
+    forecastCount: number;
+    foreCastPercentage: number;
+    cityName: string;
+  }>;
+}
+
+const initialState: WidgetState = {
+  data: [],
+  loading: false,
+};
+export const fetchWidgetData = createAsyncThunk(
+  "widgets",
+  async (_, { rejectWithValue }) => {
+    try {
+      await new Promise<void>((resolve) => {
+        setTimeout(() => {
+          resolve();
+        }, 1000);
+      });
+
+      return dataArray;
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        return rejectWithValue(error.message);
+      }
+      return rejectWithValue("An unknown error occurred");
+    }
+  }
+);
+
+export const widgetSlice = createSlice({
+  name: "chartDetailPage",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchWidgetData.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(fetchWidgetData.fulfilled, (state, action) => {
+        state.data = action.payload;
+        state.loading = false;
+      })
+      .addCase(fetchWidgetData.rejected, (state) => {
+        state.loading = false;
+      });
+  },
+});
+
+export default widgetSlice.reducer;
